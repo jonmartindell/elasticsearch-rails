26ed08b0dd57efd28bd2c78042ce89722afbed93
jonmartindell <jonmartindell@yahoo.com>
2015-07-14 10:11:58 -0400

    Update 05-settings-files.rb

--- elasticsearch-rails/lib/rails/templates/05-settings-files.rb
+++ elasticsearch-rails/lib/rails/templates/05-settings-files.rb
@@ -2,35 +2,6 @@
 
 # (See: 01-basic.rb, 02-pretty.rb, 03-expert.rb, 04-dsl.rb)
 
-append_to_file 'README.rdoc', <<-README
-
-== [5] Settings Files
-
-The `settings-files` template refactors the `Searchable` module to load the index settings
-from an external file.
-
-README
-
-git add:    "README.rdoc"
-git commit: "-m '[05] Updated the application README'"
-
-# ----- Setup the Searchable module to load settings from config/elasticsearch/articles_settings.json
-
-gsub_file "app/models/concerns/searchable.rb",
-    /index: { number_of_shards: 1, number_of_replicas: 0 }/,
-    "File.open('config/elasticsearch/articles_settings.json')"
-
-git add:    "app/models/concerns/searchable.rb"
-git commit: "-m 'Setup the Searchable module to load settings from file'"
-
-# ----- Copy the articles_settings.json file -------------------------------------------------------
-
-copy_file File.expand_path('../articles_settings.json', __FILE__), 'config/elasticsearch/articles_settings.json'
-
-git add:    "config/elasticsearch/articles_settings.json"
-git commit: "-m 'Create the articles settings file'"
-
-# ----- Temporarily set local repo for testing ----------------------------------------------------
 
 gsub_file "Gemfile",
     %r{gem 'elasticsearch-model', git: 'git://github.com/elasticsearch/elasticsearch-rails.git'},
